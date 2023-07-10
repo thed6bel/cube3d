04553ce55d81dacf860d09a91e1ecebb90ae564c
Hugo Correia <hugo.correia.1984@gmail.com>
2023-07-10 14:42:27 +0200

    10.7.22

--- src/mandatory/ft_map_check.c
+++ src/mandatory/ft_map_check.c
@@ -6,7 +6,7 @@
 /*   By: hucorrei <hucorrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/10 11:43:35 by hucorrei          #+#    #+#             */
-/*   Updated: 2023/07/10 14:34:10 by hucorrei         ###   ########.fr       */
+/*   Updated: 2023/07/10 14:40:16 by hucorrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,13 @@ char	*ft_check_xpm()
 
 static void	ft_valide_ext(char *map)
 {
+	int fd;
 	int	end;
 
+	fd = open(map, O_RDONLY);
+	if (fd < 0)
+		ft_error("Error\nMap cannot be read or does not exist");
+	close(fd);
 	end = (ft_strlen(&map[0]) - 1);
 	if (map[end] != 'b' || map[end - 1] != 'u'
 		|| map[end - 2] != 'c' || map[end - 3] != '.')
