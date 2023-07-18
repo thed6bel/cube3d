a50338e0d514846e182c3f20396a7687f189dfb6
TheD6Bel <hugo.correia.1984@gmail.com>
2023-07-18 23:09:28 +0200

    detaille...

--- src/mandatory/main.c
+++ src/mandatory/main.c
@@ -6,7 +6,7 @@
 /*   By: thed6bel <thed6bel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/07 10:57:59 by hucorrei          #+#    #+#             */
-/*   Updated: 2023/07/18 16:20:34 by thed6bel         ###   ########.fr       */
+/*   Updated: 2023/07/18 23:05:21 by thed6bel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,22 +20,28 @@ void my_mlx_pixel_put(t_mlx *data, int x, int y, int color)
 	*(unsigned int*)dst = color;
 }
 
-void ft_floor_cloud_color(t_mlx *data)
+int rgb_to_hex(int r, int g, int b) {
+    return (r << 16) + (g << 8) + b;
+}
+
+void ft_floor_cloud_color(t_mlx *data, t_file *file)
 {
     int half_height = 480 / 2;
 
+	int hex_color_f = rgb_to_hex(file->floor.red, file->floor.green, file->floor.blue);
+	int hex_color_c = rgb_to_hex(file->ceilling.red, file->ceilling.green, file->ceilling.blue);
     for (int y = 0; y < half_height; y++)
     {
         for (int x = 0; x < 640; x++)
         {
-            my_mlx_pixel_put(data, x, y, 0x87CEEB); //bleu ciel
+            my_mlx_pixel_put(data, x, y, hex_color_c); //bleu ciel
         }
     }
     for (int y = half_height; y < 480; y++)
     {
         for (int x = 0; x < 640; x++)
         {
-            my_mlx_pixel_put(data, x, y, 0x008000); //sol vert
+            my_mlx_pixel_put(data, x, y, hex_color_f); //sol vert
         }
     }
 }
@@ -54,7 +60,7 @@ int		main(int argc, char **argv)
 		vars.win = mlx_new_window(vars.mlx, 640, 480, "Hello world!");
 		vars.img = mlx_new_image(vars.mlx, 640, 480);
 		vars.addr = mlx_get_data_addr(vars.img, &vars.bits_per_pixel, &vars.line_length, &vars.endian);
-		ft_floor_cloud_color(&vars);
+		ft_floor_cloud_color(&vars, &file);
 		mlx_put_image_to_window(vars.mlx, vars.win, vars.img, 0, 0);
 		mlx_loop(vars.mlx);
 		// ft_start_3d(&cub);
--- src/mandatory/parsing/ft_parsing.c
+++ src/mandatory/parsing/ft_parsing.c
@@ -6,7 +6,7 @@
 /*   By: thed6bel <thed6bel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/18 11:14:37 by thed6bel          #+#    #+#             */
-/*   Updated: 2023/07/18 16:24:53 by thed6bel         ###   ########.fr       */
+/*   Updated: 2023/07/18 22:53:04 by thed6bel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ int	ft_start_parse(t_file *file, char *file_path)
 {
 	if (ft_file_init(file, file_path) || ft_file_load_tiles(file))
 		return (1);
-	// if (ft_check_file_colors(file))// || ft_file_map(file))
-	// 	return (1);
+	if (ft_check_file_colors(file))// || ft_file_map(file))
+	 	return (1);
 	return (0);
 }
