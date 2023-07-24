aae3088935919efe81b8f93b3c3e47c21224de8d
Hugo Correia <hugo.correia.1984@gmail.com>
2023-07-24 10:19:17 +0200

    map

--- src/mandatory/parsing/ft_map.c
+++ src/mandatory/parsing/ft_map.c
@@ -6,7 +6,7 @@
 /*   By: hucorrei <hucorrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/19 10:01:27 by hucorrei          #+#    #+#             */
-/*   Updated: 2023/07/24 10:16:19 by hucorrei         ###   ########.fr       */
+/*   Updated: 2023/07/24 10:19:16 by hucorrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,37 +31,39 @@ int is_map_valid(char **map)
     return 1;
 }
 
-int ft_map(t_file *file)
+int	ft_map(t_file *file)
 {
-    char *line;
-    char **temp_map;
-    int i = 0;
-    int MAP_MAX_SIZE;
-    int j;
+	char	*line;
+	char	**temp_map;
+	int		i;
+	int		MAP_MAX_SIZE;
+	int		j;
 
-    MAP_MAX_SIZE = ft_map_max_size(file);
-    temp_map = (char **)malloc(sizeof(char *) * MAP_MAX_SIZE);
-    if (!temp_map)
-        return 1;
-    while ((line = get_next_line(file->fd)))
-    {
-        for (j = 0; line[j]; j++) {
-            if (line[j] != '0' && line[j] != '1' && line[j] != 'N' && line[j] != 'S' &&
-                line[j] != 'E' && line[j] != 'W' && line[j] != ' ') {
-                free(line);
-                return 1;
-            }
-        }
-        temp_map[i] = ft_strdup(line);
-        free(line);
-        i++;
-    }
-    file->map = temp_map;
-    if (!is_map_valid(file->map))
-    {
-        printf("Error: Invalid map!\n");
-        //free tout les malloc
-        return 1;
-    }
-    return 0;
+	MAP_MAX_SIZE = ft_map_max_size(file);
+	temp_map = (char **)malloc(sizeof(char *) * MAP_MAX_SIZE);
+	if (!temp_map)
+		return 1;
+	i = 0;
+	while ((line = get_next_line(file->fd)))
+	{
+		for (j = 0; line[j]; j++) {
+			if (line[j] != '0' && line[j] != '1' && line[j] != 'N' && line[j] != 'S' &&
+				line[j] != 'E' && line[j] != 'W' && line[j] != ' ')
+			{
+				free(line);
+				return 1;
+			}
+		}
+		temp_map[i] = ft_strdup(line);
+		free(line);
+		i++;
+	}
+	file->map = temp_map;
+	if (!is_map_valid(file->map))
+	{
+		printf("Error: Invalid map!\n");
+		//free tout les malloc
+		return 1;
+	}
+	return 0;
 }
